1b1121f0ca15d3caae4480651bb462cb5df26ac0
emerson <emerson>
2023-10-20 14:21:10 -0300

    removerlimites

--- src/main/java/com/pdv/domain/model/Pedido.java
+++ src/main/java/com/pdv/domain/model/Pedido.java
@@ -62,7 +62,7 @@ public class Pedido {
 			 .filter(e-> {
 					 return e.getQuantidade() < 10 && e.getQuantidade() > 5  && e.getObservacao().equals("CUIDADO");
              })
-			 .limit(2)
+
 
 			 .forEach(item->item.setDesconto(new BigDecimal(String.valueOf(item.getPrecoUnitario().multiply(BigDecimal.valueOf(0.10))))));
 
